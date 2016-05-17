362aa2e3f7eae5737ddbea1ed3fbdd352396a133
Jan Biasi <jan.biasi@namics.com>
2016-05-17 16:35:31 +0200

    Update view location

--- views/render/index.js
+++ views/render/index.js
@@ -1,3 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const path = require('path');
+const DOM = require('../../lib/DOM');
+const Settings = require('../../lib/Settings');
+const baseConfig = require(path.join(__dirname, '..', '..', 'content', 'config.json'));
+
+let settings = new Settings(`${baseConfig.settings}.json`);
+
+let viewModel = new DOM(document.body, {
+    preventAutoload: true
+}).bootstrap();
