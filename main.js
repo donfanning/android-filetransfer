e594e933b5cce17ab1cb17c14b3cf1adec2caff9
Jan Biasi <jan.biasi@namics.com>
2016-05-18 15:51:33 +0200

    Update main process

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow () {
     mainWindow = new BrowserWindow(config.window);
 
     // and load the index.html of the app.
-    mainWindow.loadURL(`file://${__dirname}/views/index.html`);
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
