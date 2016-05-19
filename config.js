d04060a828f63fdfa26f9a59c984e460bfadd850
Jan Biasi <biasijan@gmail.com>
2016-05-20 00:00:03 +0200

    Adding OSX specific configuration

--- config.js
+++ config.js
@@ -1,6 +1,17 @@
-exports = module.exports = require('rc')('androidfiletransfer', {
+'use strict';
+
+const path = require('path');
+
+exports = module.exports = {
     window: {
         width: 800,
-        height: 600
+        height: 600,
+        title: 'Android Filetransfer',
+        resizable: true,
+        center: true,
+        icon: path.join(__dirname, 'dist/assets/img/appicon.png')
+    },
+    osx: {
+        icon: path.join(__dirname, 'dist/assets/img/appicon.png')
     }
-});
+};
